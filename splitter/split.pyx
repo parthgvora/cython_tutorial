821463fa1fa6756697285c1c93ee6ce7373a2c86
parthgvora <parthgvora@gmail.com>
2021-01-28 11:26:37 -0500

    nogil version of split using C++ hashmap

--- splitter/split.pyx
+++ splitter/split.pyx
@@ -3,21 +3,65 @@ cimport cython
 import numpy as np
 DTYPE = np.float64
 
-#from libc.stdlib cimport qsort
-
-"""
-cdef int cmpf(const void* a, const void* b) nogil:
-    if a < b:
-        return -1
-    elif a == b:
-        return 0
-    else:
-        return 1
-
-"""
+from libcpp.unordered_map cimport unordered_map
+from cython.operator import dereference, postincrement
 
 # Computes the gini score for a split
 # 0 < t < len(y)
+@cython.boundscheck(False) #Deactivate bounds checking
+@cython.wraparound(False) #Deactivate negative indexing
+cpdef double score2(double[:] y, int t) nogil:
+    cdef double length = y.shape[0]
+    cdef double left_gini = 1.0
+    cdef double right_gini = 1.0
+    cdef double gini = 0
+    cdef int i = 0
+    cdef double temp = 0.0
+    cdef double count = 0.0
+    
+    cdef double[:] left = y[:t]
+    cdef double[:] right = y[t:]
+
+    cdef int l_length = left.shape[0]
+    cdef int r_length = right.shape[0]
+    cdef double l_len_doub = left.shape[0]
+    cdef double r_len_doub = right.shape[0]
+
+    cdef unordered_map[double, double] count_left
+    cdef unordered_map[double, double] count_right
+
+    cdef unordered_map[double, double].iterator left_it = count_left.begin()
+    cdef unordered_map[double, double].iterator right_it = count_right.begin()
+
+    # Count all unique elements, store in hashmap
+    for i in range(0, l_length):
+        temp = left[i]
+        count_left[temp] += 1
+
+    for i in range(0, r_length):
+        temp = right[i]
+        count_right[temp] += 1
+   
+    # Compute left gini and right gini
+    left_it = count_left.begin()
+    while (left_it != count_left.end()):
+        count = dereference(left_it).second
+        count = count / l_len_doub
+        count = count * count
+        left_gini -= count
+        postincrement(left_it)
+
+    right_it = count_right.begin()
+    while (right_it != count_right.end()):
+        count = dereference(right_it).second
+        count = count / r_len_doub
+        count = count * count
+        right_gini -= count
+        postincrement(right_it)
+
+    gini = (l_length / length) * left_gini + (r_length / length) * right_gini
+    return gini
+
 @cython.boundscheck(False) #Deactivate bounds checking
 @cython.wraparound(False) #Deactivate negative indexing
 cpdef double score(double[:] y, int t):
