c87378daf6b0f2d87598e18b49647cc7db5e2137
parthgvora <parthgvora@gmail.com>
2021-02-15 13:32:51 -0500

    perfoance test

---
+++ splitter_test/test.py
@@ -0,0 +1,28 @@
+from proglearn.transformers import ObliqueTreeClassifier as OTC
+import numpy as np
+from time import time
+
+N = [100, 200, 400, 800, 1600, 3200]
+d = 10
+reps = 5
+
+print("N,time")
+
+for n in N:
+
+    for r in range(reps):
+
+        X = np.random.rand(n, d)
+        y = np.random.randint(20, size=n)
+        clf = OTC(random_state=0)
+
+        start_time = time()
+        clf.fit(X, y)
+        end_time = time()
+
+        print(n, end_time - start_time)
+
+
+
+
+
