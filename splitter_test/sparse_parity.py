20e1621a8b678674e96bb557aa93244a7197ee6e
parthgvora <parthgvora@gmail.com>
2021-02-28 10:35:37 -0500

    dataset

--- splitter_test/sparse_parity.py
+++ splitter_test/sparse_parity.py
@@ -1,28 +1,54 @@
 import sys
 import numpy as np
 import pandas as pd
-from proglearn import LifelongClassificationForest as lcf
+import sporfdata as sd
+#from proglearn import LifelongClassificationForest as lcf
+#from sklearn.ensemble import RandomForestClassifier as rfc
+
+from rerf.rerfClassifier import rerfClassifier as rfc
 
 def load_data(n):
 
-  # They are labelled backwards!
   ftrain = "Sparse_parity_test.csv"
-  ftest = "Sparse_parity_train.csv"
+  ftest = "Sparse_parity_test.csv"
 
-  df_train = pd.read_csv(ftrain, header=None).to_numpy()
+  #df_train = pd.read_csv(ftrain, header=None).to_numpy()
   df_test = pd.read_csv(ftest, header=None).to_numpy()
 
-  idx = np.random.choice(10000, n, replace=False)
+  #idx = np.random.choice(10000, n, replace=False)
 
-  X_train = df_train[idx, :-1]
-  y_train = df_train[idx, -1]
+  #X_train = df_train[idx, :-1]
+  #y_train = df_train[idx, -1]
 
   X_test = df_test[:, :-1]
   y_test = df_test[:, -1]
 
+  X_train, y_train = sd.sparse_parity(n)
+
   return X_train, y_train, X_test, y_test
 
-def test(n, reps, n_estimators, feature_combinations, density):
+def test_rf(n, reps, n_estimators):
+
+  preds = np.zeros((reps, 10000))
+  acc = np.zeros(reps)
+  for i in range(reps):
+
+    X_train, y_train, X_test, y_test = load_data(n)
+
+    clf = rfc(n_estimators=n_estimators,
+              projection_matrix="Base",
+              oob_score=False)
+              #max_features=10)
+    clf.fit(X_train, y_train)
+    
+    
+    preds[i] = clf.predict(X_test)
+    acc[i] = np.sum(preds[i] == y_test) / len(y_test)
+
+  np.save("output/rf_sparse_parity_preds_" + str(n) + ".npy", preds)
+  return acc
+
+def test_lcf(n, reps, n_estimators, feature_combinations, density):
 
   preds = np.zeros((reps, 1000))
   acc = np.zeros(reps)
@@ -52,7 +78,8 @@ def main():
   feature_combinations = 1.5
   density = 0.25
 
-  acc = test(n, reps, n_estimators, feature_combinations, density)
+  #acc = test_lcf(n, reps, n_estimators, feature_combinations, density)
+  acc = test_rf(n, reps, n_estimators)
   print(acc)
 
 if __name__ == "__main__":
